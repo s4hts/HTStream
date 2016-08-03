2e88156dabda57d720876eb49ba8b4ac13e578c7
Joe Angell <joe@boulderlabs.com>
2016-08-03 12:39:50 -0700

    hooked up start and length arguments

--- Super-Deduper/src/super-deduper.cpp
+++ Super-Deduper/src/super-deduper.cpp
@@ -19,13 +19,13 @@ typedef std::map<std::string, int> Counter;
 typedef std::map <boost::dynamic_bitset<>, std::unique_ptr<ReadBase>> BitMap;
 
 template <class T, class Impl>
-void load_map(InputReader<T, Impl> &reader, Counter& counters, BitMap& read_map) {
+void load_map(InputReader<T, Impl> &reader, Counter& counters, BitMap& read_map, size_t start, size_t length) {
     while(reader.has_next()) {
         auto i = reader.next();
         counters["TotalRecords"]++;
         //std::cout << "read id: " << i->get_read().get_id() << std::endl;
         //check for existance, store or compare quality and replace:
-        auto key=i->get_key(10, 10);
+        auto key=i->get_key(start, length);
         if(!read_map.count(key)) {
             read_map[key] = std::move(i);
         } else if(i->avg_q_score() > read_map[key]->avg_q_score()){
@@ -42,6 +42,7 @@ int main(int argc, char** argv)
     Counter counters;
     counters["TotalRecords"] = 0;
     counters["Replaced"] = 0;
+    size_t start, length = 0;
 
     try
     {
@@ -62,8 +63,8 @@ int main(int argc, char** argv)
             ("interleaved-input,I", po::value< std::vector<std::string> >(),
                                            "Interleaved input I <comma sep for multiple files>")
             ("stdin-input,S",              "STDIN input <MUST BE TAB DELIMITED INPUT>")
-            ("start,s", po::value<int>(),  "Start location for unique ID <int>")
-            ("length,l", po::value<int>(), "Length of unique ID <int>")
+            ("start,s", po::value<size_t>(&start),  "Start location for unique ID <int>")
+            ("length,l", po::value<size_t>(&length), "Length of unique ID <int>")
             ("quality-check-off,q",        "Quality Checking Off First Duplicate seen will be kept")
             ("gzip-output,g",              "Output gzipped")
             ("interleaved-output, i",      "Output to interleaved")
@@ -106,7 +107,7 @@ int main(int argc, char** argv)
                     std::ifstream read1(read1_files[i], std::ifstream::in);
                     std::ifstream read2(read2_files[i], std::ifstream::in);
                     InputReader<PairedEndRead, PairedEndReadImpl> ifp(read1, read2);
-                    load_map(ifp, counters, read_map);
+                    load_map(ifp, counters, read_map, start, length);
                 }
             }
             else if(vm.count("singleend-input")) {
@@ -114,7 +115,7 @@ int main(int argc, char** argv)
                 for (auto file : read_files) {
                     std::ifstream read1(file, std::ifstream::in);
                     InputReader<SingleEndRead, SingleEndReadImpl> ifs(read1);
-                    load_map(ifs, counters, read_map);
+                    load_map(ifs, counters, read_map, start, length);
                 }
             }
             
