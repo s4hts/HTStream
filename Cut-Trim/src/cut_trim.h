efed7c8913bf67c40f7dfa753658bdfaae8d8db0
dstreett <streett.david@gmail.com>
2016-10-11 17:19:44 -0700

    Static trimmer

---
+++ Cut-Trim/src/cut_trim.h
@@ -0,0 +1,56 @@
+#ifndef AT_TRIM_H
+#define AT_TRIM_H
+//  this is so we can implment hash function for dynamic_bitset
+#define BOOST_DYNAMIC_BITSET_DONT_USE_FRIENDS
+
+#include "ioHandler.h"
+#include <map>
+#include <unordered_map>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/functional/hash.hpp>
+#include <algorithm>
+
+typedef std::unordered_map<std::string, size_t> Counter;
+
+template <class T, class Impl>
+void helper_trim(InputReader<T, Impl> &reader, std::shared_ptr<OutputWriter> pe, std::shared_ptr<OutputWriter> se, Counter& counters, size_t min_length, size_t cut_size, bool stranded, bool no_left, bool no_right) {
+    
+    while(reader.has_next()) {
+        auto i = reader.next();
+        ++counters["TotalRecords"];
+        PairedEndRead* per = dynamic_cast<PairedEndRead*>(i.get());        
+        if (per) {
+            Read &rb1 = per->non_const_read_one();
+            Read &rb2 = per->non_const_read_one();
+            if (!no_left) {
+                rb1.setLCut(cut_size);
+                rb2.setLCut(cut_size);
+            }
+            if (!no_right) {
+                rb1.setRCut(rb1.getLength() - cut_size);
+                rb2.setRCut(rb2.getLength() - cut_size);
+            }
+            per->checkDiscarded(min_length);
+            writer_helper(per, pe, se, stranded);
+        } else {
+            SingleEndRead* ser = dynamic_cast<SingleEndRead*>(i.get());
+            
+            if (ser) {
+                Read &rb = per->non_const_read_one();
+                if (!no_left) {
+                    rb.setLCut(cut_size);
+                } 
+                if (!no_right) {
+                    rb.setRCut((per->non_const_read_two()).getLength() - cut_size);
+                }
+                ser->checkDiscarded(min_length);
+                writer_helper(ser, pe, se, stranded);
+            } else {
+                throw std::runtime_error("Unknow read type");
+            }
+        }
+    }
+
+}
+
+#endif
---
+++ Cut-Trim/test/testCutTrim.cpp
@@ -0,0 +1,58 @@
+#include "gtest/gtest.h"
+#include <sstream>
+#include <iostream>
+#include "cut_trim.h"
+
+class SDTest : public ::testing::Test {
+    public:
+        const std::string readData_1 = "@Read1\nGGGGGGGGGGCAAAAAAAACGGGGGGGGGG\n+\n##############################\n";
+        const std::string readData_2 = "@Read1\nAAAAAAAAAAAAAAAAAAAAAAAA\n+\n########################\n";
+        size_t min_length = 5;
+        size_t mega_min_length = 10000;
+        size_t cut_size = 10;
+};
+
+TEST_F(SDTest, BasicTrim) {
+    std::istringstream in1(readData_1);
+    std::istringstream in2(readData_2);
+
+    InputReader<PairedEndRead, PairedEndReadFastqImpl> ifp(in1, in2);
+
+    while(ifp.has_next()) {
+        auto i = ifp.next();
+        PairedEndRead *per = dynamic_cast<PairedEndRead*>(i.get());
+        Read &rb1 = per->non_const_read_one();
+        Read &rb2 = per->non_const_read_two();
+        rb1.setLCut(cut_size);
+        rb1.setRCut(rb1.getLength() - cut_size);
+        ASSERT_EQ("CAAAAAAAAC", (per->non_const_read_one()).get_sub_seq());
+    }
+};
+
+TEST_F(SDTest, Stranded) {
+    std::istringstream in1(readData_2); //REverse these two so R1 is discarded and R2 is RCed
+    std::istringstream in2(readData_1);
+
+    InputReader<PairedEndRead, PairedEndReadFastqImpl> ifp(in1, in2);
+    std::shared_ptr<std::ostringstream> out1(new std::ostringstream);
+
+    {
+        std::shared_ptr<HtsOfstream> hts_of(new HtsOfstream(out1));
+        std::shared_ptr<OutputWriter> tab(new ReadBaseOutTab(hts_of));
+        while(ifp.has_next()) {
+            auto i = ifp.next();
+            PairedEndRead *per = dynamic_cast<PairedEndRead*>(i.get());
+            Read &rb1 = per->non_const_read_one();
+            Read &rb2 = per->non_const_read_two();
+
+            rb1.setLCut(cut_size);
+            rb1.setRCut(rb1.getLength() - cut_size);
+            rb2.setLCut(cut_size);
+            rb2.setRCut(rb2.getLength() - cut_size);
+            per->checkDiscarded(min_length);
+            writer_helper(per, tab, tab, true);
+        }
+    }
+    ASSERT_EQ("Read1\tGTTTTTTTTG\t##########\n", out1->str());
+};
+
