6cc794002f15d0d94fba8e16ec4e74a16f8c14b7
dstreett <streett.david@gmail.com>
2016-11-18 08:52:02 -0800

    First commit to overlap

---
+++ Overlapper/src/overlapper.h
@@ -0,0 +1,203 @@
+/*The idea of this program is to create a quick kmer lookup table for phix.
+ * It accomplishes this by making 2 arrays of 1<<16 size, because we represent
+ * each possible kmer in the 2 bit format (A -> 00, T -> 11, C -> 01, G->10) and
+ * need foward and Reverse strands. Each read is then parsed into 8mers (again
+ * represented by their 2 bit format, and checked against these lookup tables
+ * (simply the arrays)*/ 
+
+#ifndef AT_TRIM_H
+#define AT_TRIM_H
+//  this is so we can implment hash function for dynamic_bitset
+#define BOOST_DYNAMIC_BITSET_DONT_USE_FRIENDS
+
+#include "ioHandler.h"
+#include <map>
+#include <unordered_map>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/functional/hash.hpp>
+#include <algorithm>
+#include <bitset>
+#include <utility>
+
+typedef std::unordered_map<std::string, size_t> Counter;
+typedef std::multimap<std::string, std::size_t> seqLookup;
+typedef std::shared_ptr<SingleEndRead> spReadBase;
+
+const size_t kmer = 8;
+const size_t kmerBits = kmer*2;
+const size_t step = 2;
+
+uint8_t getBin(char c) {
+    if (c == 'A') 
+        return 0;
+    else if (c == 'T')
+        return 3;
+    else if (c == 'C')
+        return 1;
+    else if (c == 'G')
+        return 2;
+    return 5;
+}
+
+seqLookup readOneMap(const std::string &seq1, size_t step) {
+   /* uint16_t forwardBits = 0;
+    size_t kmerCheck = kmer - 1;
+    uint8_t bin;*/
+
+
+    seqLookup baseReadMap;
+    std::cout << seq1 << '\n';
+    size_t seqLen = seq1.length();
+    /*8 is a random number*/
+    for (size_t bp = 0; bp < seqLen - kmer; bp+=kmer) {
+        std::cout << seq1.substr(bp, kmer) << '\n';
+        baseReadMap.insert(std::make_pair(seq1.substr(bp, kmer), bp));
+       /* bin = getBin(seq1[bp]);
+        if (bin != 5) {
+            forwardBits &= ~(3 << 14); //only check forward strand against both RC and non-rc
+            forwardBits <<= 2;
+            forwardBits ^= bin;
+        } else {
+            kmerCheck = kmer - 1;
+        }
+
+        if (!kmerCheck) {
+        } else {
+            --kmerCheck;    
+        }*/
+    }
+    baseReadMap.insert(std::make_pair(seq1.substr(seqLen - kmer, kmer), seqLen - kmer));
+    std::cout << seq1.substr(seqLen - kmer, kmer) << '\n';
+
+    return baseReadMap;
+}
+
+spReadBase check(Read &r1, Read &r2, const size_t &loc1, const size_t &loc2, const size_t &maxMis, const size_t &minOverlap) {
+    size_t minLoc = std::min(loc1, loc2);
+    size_t loc1_t = loc1 - minLoc;
+    size_t loc2_t = loc2 - minLoc;
+    size_t maxLoop = std::min(r1.getLength() - loc1_t, r2.getLength() - loc2_t);
+    
+    if (maxLoop <= minOverlap) {
+        std::cout << "NULL\n";
+        return nullptr;
+    }
+
+    size_t read1_bp;
+    size_t read2_bp;
+
+    const std::string &seq1 = r1.get_seq();
+    const std::string &seq2 = r2.get_seq_rc();
+    const std::string &qual1 = r1.get_qual();
+    const std::string &qual2 = r2.get_qual_rc();
+    std::string finalSeq;
+    std::string finalQual;
+    size_t misMatches = 0;
+    std::cout << "Loc1 " << loc1_t << " Loc2 " << loc2_t << '\n';
+
+    if ( loc1_t >= loc2_t) {
+        std::cout << "SNAG FIRST HALF\n";
+        finalSeq += seq1.substr(0, loc1_t);
+        finalQual += qual1.substr(0, loc1_t);
+    }
+
+    for (size_t i = 0; i < maxLoop; ++i) {
+        read1_bp = loc1_t + i;
+        read2_bp = loc2_t + i;
+        if (seq1[read1_bp] == seq2[read2_bp]) {
+            finalSeq += seq1[read1_bp];
+            finalQual += std::min(qual1[read1_bp] + qual2[read2_bp] - 33, 40 + 33); //addition of qual (minus just one of the ascii values 
+        } else {
+            finalSeq += qual1[read1_bp] > qual2[read2_bp] ? seq1[read1_bp] : seq2[read2_bp];
+            finalQual += std::max(qual1[read1_bp] - qual2[read2_bp] + 33, 1 + 33);
+            ++misMatches;
+        }
+
+    }
+    
+    if (r1.getLength() - loc1_t < r2.getLength()) {
+        std::cout << "Snag second half\n";
+        finalSeq += seq2.substr(maxLoop, r2.getLength() - maxLoop);
+        finalQual += seq2.substr(maxLoop, r2.getLength() - maxLoop);
+    }
+    std::cout << "Final\n";
+    std::cout << finalSeq << '\n';
+    std::cout << finalQual << '\n';
+    return spReadBase(new SingleEndRead(Read(finalSeq, finalQual, "ID")));
+}
+
+spReadBase getOverlappedReads(Read &r1, Read &r2, const seqLookup &seq1Map) {
+    uint16_t forwardBits = 0;
+    size_t kmerCheck = kmer - 1;
+    uint8_t bin;
+    std::vector< std::pair<size_t, size_t> > loc;
+    std::cout << "Start\n";
+    std::cout << r2.get_seq_rc() << '\n';
+    for (size_t bp = 0; bp < r2.getLength() - kmer; ++bp) {
+        /*bin = getBin(seq2[bp]);
+        if (bin != 5) {
+            forwardBits &= ~(3 << 14); //only check forward strand against both RC and non-rc
+            forwardBits <<= 2;
+            forwardBits ^= bin;
+        } else {
+           kmerCheck = kmer - 1;
+        }
+        if (!kmerCheck) {
+            auto test = seq1Map.equal_range(seq2.substr(bp, kmer));
+            for (auto it = test.first; it != test.second; ++it) {
+                check(seq)
+                loc.push_back(std::make_pair((*it).second, bp));
+            }
+        } else {
+            --kmerCheck;
+        }*/
+        auto test = seq1Map.equal_range(r2.get_seq_rc().substr(bp, kmer));
+        std::cout << r2.get_seq_rc().substr(bp, kmer) << '\n';
+        for (auto it = test.first; it != test.second; ++it) {
+            std::cout << "HERE\n";
+            check(r1, r2, (*it).second, bp, 0, 0);
+        }
+    } 
+
+    return nullptr;
+
+}
+
+spReadBase check_read(PairedEndRead &pe , const size_t maxMis, const size_t minOver) {
+    
+    Read &r1 = pe.non_const_read_one();
+    Read &r2 = pe.non_const_read_two();
+
+    if (r1.getLength() < r2.getLength()) {
+        std::swap(r1, r2);
+    }
+    seqLookup mOne = readOneMap(r1.get_seq(), step);
+    spReadBase overlappedRead = getOverlappedReads(r1, r2, mOne) ;
+    
+}
+
+
+template <class T, class Impl>
+void helper_overlapper(InputReader<T, Impl> &reader, std::shared_ptr<OutputWriter> pe, std::shared_ptr<OutputWriter> se, Counter& counters, size_t hits) {
+    
+    while(reader.has_next()) {
+        auto i = reader.next();
+        ++counters["TotalRecords"];
+        PairedEndRead* per = dynamic_cast<PairedEndRead*>(i.get());        
+        if (per) {
+            pe->write(*per);
+
+        } else {
+            SingleEndRead* ser = dynamic_cast<SingleEndRead*>(i.get());
+            
+            if (ser) {
+                se->write(*ser);
+            } else {
+                throw std::runtime_error("Unknown read type");
+            }
+        }
+    }
+}
+
+#endif
+
---
+++ Overlapper/test/testoverlapper.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+#include <sstream>
+#include <iostream>
+#include "overlapper.h"
+
+class Overlapper : public ::testing::Test {
+    public:
+        
+        const std::string readData_perfect_overlap_R1 = "@R1_perfect\nACTTGACATTAAGCAAGTACCAGTACCGATACCATAGGACCCAAGGTA\n+\n111111111111111111111111111111111111111111111111\n";
+        const std::string readData_perfect_overlap_R2 = "@R2_perfect\nTACCTTGGGTCCTATGGTATCGGTACTGGTACTTGCTTAATGTCAAGT\n+\n111111111111111111111111111111111111111111111111\n";
+        
+        const std::string readData_normal_overlap_R1 = "@R1_\nGGTAAACCATTAACCAAATTGGACATATCTCATCTATCCTTATACTTA\n+\n111111111111111111111111111111111111111111111111\n";
+        const std::string readData_normal_overlap_R2 = "@R2_\nCTCTCGGTCTCCTCTCGTTTCTCGTTCGCGCTAAGTATAAGGATAGA\n+\n11111111111111111111111111111111111111111111111\n";
+        
+        const std::string readData_trim_overlap_R1 = "@R1_\nACCATAACATAAACCATTAACCAAATTGGACATATCTCATCTATCCTTATACTTA\n+\n1111111111111111111111111111111111111111111111111111111\n";
+        const std::string readData_trim_overlap_R2 = "@R2_\nGGTTTATGTTATGGTAATATAGTATAGAGTATAGTTGCGTC\n+\n11111111111111111111111111111111111111111\n";
+
+        const std::string readData_engulf_r2_R1 = "@R1_\nGGTAAACCATTAACCAAATTGGACATATCTCATCTATCCTTATACTTA\n+\n111111111111111111111111111111111111111111111111\n";
+        const std::string readData_engulf_r2_R2 = "@R2_\nAGATGAGATATGTCCAATTTGGTTAATGGTT\n+\n1111111111111111111111111111111";
+        
+        const std::string readData_engulf_r1_R1 = "@R1_\nAACCATTAACCAAATTGGACATATCTCATCT\n+\n1111111111111111111111111111111\n";
+        const std::string readData_engulf_r1_R2 = "@R2_\nTAAGTATAAGGATAGATGAGATATGTCCAATTTGGTTAATGGTTTACC\n+\n111111111111111111111111111111111111111111111111\n";
+};
+
+TEST_F(Overlapper, perfectOverlap) {
+    std::istringstream in1(readData_perfect_overlap_R1);
+    std::istringstream in2(readData_perfect_overlap_R2);
+
+    InputReader<PairedEndRead, PairedEndReadFastqImpl> ifp(in1, in2);
+
+    while(ifp.has_next()) {
+        auto i = ifp.next();
+        PairedEndRead *per = dynamic_cast<PairedEndRead*>(i.get());
+        spReadBase r = check_read(*per, 100, 100);
+    }
+    std::cout << (*r).getSeq() << '\n';
+};
+
