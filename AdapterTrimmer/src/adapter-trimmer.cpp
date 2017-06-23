c6b0ea6b27bc2f3d2f24ef990359ff4478091f5a
David Streett <streett.david@gmail.com>
2017-06-23 10:32:52 -0700

    AdapterTrimmer online - adapter trimmer will only return PE reads if a PE is entered

---
+++ AdapterTrimmer/src/adapter-trimmer.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+#include <vector>
+#include <fstream>
+#include "ioHandler.h"
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem.hpp>
+#include <map>
+#include <unordered_map>
+#include <boost/functional/hash.hpp>
+
+#include "adapter-trimmer.h"
+
+#define AST_COUNT 4096 //number of reads per one *
+
+namespace
+{
+    const size_t SUCCESS = 0;
+    const size_t ERROR_IN_COMMAND_LINE = 1;
+    const size_t ERROR_UNHANDLED_EXCEPTION = 2;
+
+} // namespace
+
+namespace bi = boost::iostreams;
+
+int main(int argc, char** argv)
+{
+    const std::string program_name = "Overlapper";
+
+    Counter counters;
+    setupCounter(counters); 
+
+    try
+    {
+        /** Define and parse the program options
+         */
+        namespace po = boost::program_options;
+        po::options_description desc("Options");
+
+        setDefaultParams(desc, program_name);
+        setDefaultParamsCutting(desc);
+        setDefaultParamsTrim(desc);
+
+        desc.add_options()
+            ("kmer,k", po::value<size_t>()->default_value(8), "Kmer size of the lookup table for the longer read")
+            ("kmer-offset,r", po::value<size_t>()->default_value(1), "Offset of kmers. Offset of 1, would be perfect overlapping kmers. An offset of kmer would be non-overlapping kmers that are right next to each other. Must be greater than 0.")
+            ("max-mismatch-errorDensity,x", po::value<double>()->default_value(.25), "Max percent of mismatches allowed in overlapped section")
+            ("check-lengths,c", po::value<size_t>()->default_value(20), "Check lengths on the ends")
+            ("min-overlap,o", po::value<size_t>()->default_value(8), "Min overlap required to merge two reads")
+           ("hist-file,e", po::value<std::string>()->default_value(""), "A tab delimited hist file with insert lengths.");
+
+                   po::variables_map vm;
+        try
+        {
+            po::store(po::parse_command_line(argc, argv, desc),
+                      vm); // can throw
+            
+            version_or_help(program_name, desc, vm);
+
+            po::notify(vm); // throws on error, so do after help in case
+         
+            if (vm["max-mismatch-errorDensity"].as<double>() < 0.0 ||  vm["max-mismatch-errorDensity"].as<double>() > 1.0) {
+                throw std::runtime_error("Woah, there human. It seems you have entered a wacky, zany number that isn't between 0.0 and 1.0 for max mismatch errorDensity.\nThat is why there is this error message. (>^_^)>\n\n");
+            }
+
+            if (vm["kmer-offset"].as<size_t>() == 0) {
+                throw std::runtime_error("Human - you cannot have a kmer offset of zero! Madness would ensue. Please, try again, but this time make sure the kmer-offset,r flag is set to above 0.\n\n");
+            }
+        
+            std::string statsFile(vm["stats-file"].as<std::string>());
+            std::string prefix(vm["prefix"].as<std::string>());
+   
+            std::shared_ptr<OutputWriter> pe = nullptr;
+            std::shared_ptr<OutputWriter> se = nullptr;
+            
+            outputWriters(pe, se, vm["fastq-output"].as<bool>(), vm["tab-output"].as<bool>(), vm["interleaved-output"].as<bool>(), vm["unmapped-output"].as<bool>(), vm["force"].as<bool>(), vm["gzip-output"].as<bool>(), vm["to-stdout"].as<bool>(), prefix );
+            histVec insertLengths;
+
+            if (vm["hist-file"].as<std::string>() == "") {
+                insertLengths = nullptr;
+            } else {
+                insertLengths = histVec(new std::vector<unsigned long long int>);
+            }
+            
+
+            if(vm.count("read1-input")) {
+                if (!vm.count("read2-input")) {
+                    throw std::runtime_error("must specify both read1 and read2 input files.");
+                } else if (vm.count("read2-input") != vm.count("read1-input")) {
+                    throw std::runtime_error("must have same number of input files for read1 and read2");
+                }
+                auto read1_files = vm["read1-input"].as<std::vector<std::string> >();
+                auto read2_files = vm["read2-input"].as<std::vector<std::string> >();
+
+                for(size_t i = 0; i < read1_files.size(); ++i) {
+                    bi::stream<bi::file_descriptor_source> is1{check_open_r(read1_files[i]), bi::close_handle};
+                    bi::stream<bi::file_descriptor_source> is2{check_open_r(read2_files[i]), bi::close_handle};
+                    InputReader<PairedEndRead, PairedEndReadFastqImpl> ifp(is1, is2);
+                    helper_overlapper(ifp, pe, se, counters, vm["max-mismatch-errorDensity"].as<double>(),  vm["min-overlap"].as<size_t>(), insertLengths, vm["stranded"].as<bool>(), vm["min-length"].as<size_t>(), vm["check-lengths"].as<size_t>(), vm["kmer"].as<size_t>(), vm["kmer-offset"].as<size_t>(), vm["no-orphans"].as<bool>() ); }
+            }
+
+            if(vm.count("singleend-input")) {
+                auto read_files = vm["singleend-input"].as<std::vector<std::string> >();
+                for (auto file : read_files) {
+                    bi::stream<bi::file_descriptor_source> sef{ check_open_r(file), bi::close_handle};
+                    InputReader<SingleEndRead, SingleEndReadFastqImpl> ifs(sef);
+                    //JUST WRITE se read out - no way to overlap
+                    helper_overlapper(ifs, pe, se, counters, vm["max-mismatch-errorDensity"].as<double>(),  vm["min-overlap"].as<size_t>(), insertLengths, vm["stranded"].as<bool>(), vm["min-length"].as<size_t>(), vm["check-lengths"].as<size_t>(), vm["kmer"].as<size_t>(), vm["kmer-offset"].as<size_t>(), vm["no-orphans"].as<bool>() );
+                }
+            }
+            
+            if(vm.count("tab-input")) {
+                auto read_files = vm["tab-input"].as<std::vector<std::string> > ();
+                for (auto file : read_files) {
+                    bi::stream<bi::file_descriptor_source> tabin{ check_open_r(file), bi::close_handle};
+                    InputReader<ReadBase, TabReadImpl> ift(tabin);
+                    helper_overlapper(ift, pe, se, counters, vm["max-mismatch-errorDensity"].as<double>(),  vm["min-overlap"].as<size_t>(), insertLengths, vm["stranded"].as<bool>(), vm["min-length"].as<size_t>(), vm["check-lengths"].as<size_t>(), vm["kmer"].as<size_t>(), vm["kmer-offset"].as<size_t>(), vm["no-orphans"].as<bool>() );
+                }
+            }
+            
+            if (vm.count("interleaved-input")) {
+                auto read_files = vm["interleaved-input"].as<std::vector<std::string > >();
+                for (auto file : read_files) {
+                    bi::stream<bi::file_descriptor_source> inter{ check_open_r(file), bi::close_handle};
+                    InputReader<PairedEndRead, InterReadImpl> ifp(inter);
+                    helper_overlapper(ifp, pe, se, counters, vm["max-mismatch-errorDensity"].as<double>(),  vm["min-overlap"].as<size_t>(), insertLengths, vm["stranded"].as<bool>(), vm["min-length"].as<size_t>(), vm["check-lengths"].as<size_t>(), vm["kmer"].as<size_t>(), vm["kmer-offset"].as<size_t>(), vm["no-orphans"].as<bool>() );
+                }
+            }
+           
+            if (vm.count("std-input")) {
+                bi::stream<bi::file_descriptor_source> tabin {fileno(stdin), bi::close_handle};
+                InputReader<ReadBase, TabReadImpl> ift(tabin);
+                helper_overlapper(ift, pe, se, counters, vm["max-mismatch-errorDensity"].as<double>(),  vm["min-overlap"].as<size_t>(), insertLengths, vm["stranded"].as<bool>(), vm["min-length"].as<size_t>(), vm["check-lengths"].as<size_t>(), vm["kmer"].as<size_t>(), vm["kmer-offset"].as<size_t>(), vm["no-orphans"].as<bool>() );
+            }  
+
+
+            if (insertLengths) {
+                std::ofstream histOutputFile(vm["hist-file"].as<std::string>() );
+                //0 is reserved for no overlap
+                std::string stars;
+                for (size_t i = 1; i < insertLengths->size(); ++i) {
+                    stars = "";
+                    if ((*insertLengths)[i]) {
+                        stars = stars.insert(0, (*insertLengths)[i]/AST_COUNT, '*');
+                        histOutputFile << i << '\t' << (*insertLengths)[i] << '\t' << stars << '\n';
+                    }
+                }
+                //No overalp at the end
+                stars = stars.insert(0, (*insertLengths)[0]/AST_COUNT, '*');
+                histOutputFile << "None" << '\t' << (*insertLengths)[0] << '\t' << stars << '\n';
+                histOutputFile.close();
+            }
+
+        }
+        catch(po::error& e)
+        {
+            std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+            std::cerr << desc << std::endl;
+            return ERROR_IN_COMMAND_LINE;
+        }
+
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << "\n\tUnhandled Exception: "
+                  << e.what() << std::endl;
+        return ERROR_UNHANDLED_EXCEPTION;
+
+    }
+
+    return SUCCESS;
+
+}
+
---
+++ AdapterTrimmer/src/adapter-trimmer.h
@@ -0,0 +1,232 @@
+/*The idea of this program is to create a quick kmer lookup table for phix.
+ * It accomplishes this by making 2 arrays of 1<<16 size, because we represent
+ * each possible kmer in the 2 bit format (A -> 00, T -> 11, C -> 01, G->10) and
+ * need foward and Reverse strands. Each read is then parsed into 8mers (again
+ * represented by their 2 bit format, and checked against these lookup tables
+ * (simply the arrays)*/ 
+
+#ifndef AT_TRIM_H
+#define AT_TRIM_H
+//  this is so we can implment hash function for dynamic_bitset
+#define BOOST_DYNAMIC_BITSET_DONT_USE_FRIENDS
+#define STARTS 4098
+
+#include "ioHandler.h"
+#include <map>
+#include <unordered_map>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/functional/hash.hpp>
+#include <algorithm>
+#include <bitset>
+#include <utility>
+#include "utils.h"
+
+typedef std::unordered_multimap<std::string, std::size_t> seqLookup;
+typedef std::shared_ptr<std::vector<unsigned long long int>> histVec;
+
+/*Create the quick lookup table
+ * Multi map because a single kemr could appear multiple places*/
+seqLookup readOneMap(std::string seq1, const size_t kmer, const size_t kmerOffset) {
+
+    seqLookup baseReadMap;
+    std::string::iterator it;
+    for ( it = seq1.begin() ; it < seq1.end() - ( static_cast<long> ( kmerOffset + kmer ) ) ; it += static_cast<long> ( kmerOffset )   ) {
+        baseReadMap.insert(std::make_pair( std::string ( it, it+ static_cast<long> ( kmer )  ) , it - seq1.begin() ));
+    }
+
+    if ( seq1.begin() + static_cast<long> (kmer) > seq1.end() ) {
+        it = seq1.end() - static_cast<long> ( kmer );
+        baseReadMap.insert(std::make_pair(  std::string( it , it + static_cast<long> (kmer)), it - seq1.begin()   ) );
+    }
+
+    return baseReadMap;
+}
+/*If adapater trimming is turned on that means adapter trimming and do not overlap
+ * so trim adapter, but don't worry about the overlap.
+ * however we still need to change the overlap
+ * 
+ * Within the overlap if they are the same bp, then add q scores
+ * If they are different bp, subtract q scores and take the larger quality bp*/ 
+unsigned int checkIfOverlap(Read &r1, Read &r2, size_t loc1, size_t loc2, const double misDensity, size_t minOverlap) {
+    size_t minLoc = std::min(loc1, loc2);
+    size_t loc1_t = loc1 - minLoc;
+    size_t loc2_t = loc2 - minLoc;
+    size_t maxLoop = std::min(r1.getLength() - loc1_t, r2.getLength() - loc2_t);
+    size_t maxMis = static_cast<size_t>(maxLoop * misDensity);
+
+    if (maxLoop <= minOverlap) {
+        return 0;
+    }
+
+    size_t read1_bp;
+    size_t read2_bp;
+
+    const std::string &seq1 = r1.get_seq();
+    const std::string &seq2 = r2.get_seq_rc();
+    const std::string &qual1 = r1.get_qual();
+    const std::string &qual2 = r2.get_qual_rc();
+
+    std::string finalSeq;
+    std::string finalQual;
+    size_t misMatches = 0;
+
+    char bp;
+    char qual;
+
+    unsigned int insert_length = maxLoop;
+    for (size_t i = 0; i < maxLoop; ++i) {
+        read1_bp = loc1_t + i;
+        read2_bp = loc2_t + i;
+        
+        if (seq1[read1_bp] == seq2[read2_bp]) {
+            bp = seq1[read1_bp];
+            qual = static_cast<char>(std::min(qual1[read1_bp] + qual2[read2_bp] - 33, 40 + 33)); //addition of qual (minus just one of the ascii values 
+        } else {
+            bp = qual1[read1_bp] > qual2[read2_bp] ? seq1[read1_bp] : seq2[read2_bp];
+            qual = static_cast<char>(std::max(qual1[read1_bp] - qual2[read2_bp] + 33, 1 + 33));
+            
+            if (++misMatches > maxMis) {
+                /*Not valid match*/
+                return 0;
+            }
+        }
+        r1.changeSeq(read1_bp, bp);
+        r1.changeQual(read1_bp, qual);
+        r2.changeSeq(read2_bp, bp);
+        r2.changeQual(read2_bp, qual);
+        
+       
+    }
+
+    if ( loc1_t >= loc2_t ) { //no adapter but need to figure out sides
+        insert_length += loc1_t; 
+    } else {
+        r1.setLCut(loc1_t); //sin
+    }
+    
+    if (r1.getLength() - loc1_t < r2.getLength()) {
+        insert_length += r2.getLength() - maxLoop; // lin
+    } else {
+        r2.setRCut(maxLoop); //sin
+    }
+
+    return insert_length;
+}
+
+/*Because of the way overlapping works, you only need to check the ends of the shorter read*/
+unsigned int getOverlappedReads(Read &r1, Read &r2, const seqLookup &seq1Map,  const double misDensity, const size_t &minOver, const size_t &checkLengths, const size_t kmer) {
+
+    std::string seq2 = r2.get_seq_rc();
+    for (size_t bp = 0; bp < checkLengths; ++bp) {
+        /*Do a quick check if the shorter read kmer shows up in longer read (read 2)
+         * If it does, then try the brute force approach*/
+        auto test = seq1Map.equal_range(seq2.substr(bp, kmer));
+        for (auto it = test.first; it != test.second; ++it) {
+            unsigned int overlapped = checkIfOverlap(r1, r2, it->second, bp, misDensity, minOver);
+            if (overlapped) {
+                return overlapped;
+            }
+        }
+    } 
+
+    for (size_t bp = seq2.length() - (checkLengths + kmer); bp < seq2.length() - kmer ; ++bp) {
+        /*Do a quick check if the shorter read kmer shows up in longer read (read 2)
+         * If it does, then try the brute force approach*/
+        auto test = seq1Map.equal_range(seq2.substr(bp, kmer));
+        for (auto it = test.first; it != test.second; ++it) {
+            unsigned int overlapped = checkIfOverlap(r1, r2, it->second, bp, misDensity, minOver);
+            if (overlapped) {
+                return overlapped;
+            }
+        }
+    }
+    return 0;
+
+}
+
+unsigned int check_read(PairedEndRead &pe , Counter &counters, const double misDensity, const size_t &minOver, histVec &insertLength, const size_t &checkLengths, const size_t kmer, const size_t kmerOffset, const size_t minLength) {
+    
+    Read &r1 = pe.non_const_read_one();
+    Read &r2 = pe.non_const_read_two();
+
+    bool swapped = false;
+    /*Read1 is always longer than Read 2)*/
+    if (r1.getLength() < r2.getLength()) {
+        std::swap(r1, r2);
+        swapped = true;
+    }
+    /*Create a map with non-overlapping kmers*/
+    seqLookup mOne = readOneMap(r1.get_seq(), kmer, kmerOffset);
+    /*returns null if no much
+     * r1 and r2 and passed by ref in case only adapter trimming is on*/
+    unsigned int overlapped = getOverlappedReads(r1, r2, mOne, misDensity, minOver, checkLengths, kmer) ;
+    //we need to check it overlapper is greater than min length;
+
+    if (overlapped < minLength) {
+        if (insertLength) { /*No overlap*/
+            ++(*insertLength)[0];
+        }
+        return 0;
+    }
+
+    if (overlapped) {
+        if (insertLength) { //overlap plus writing out
+            /*This is important for the hist file
+            * Shows distribution of lins and sins*/
+            if (insertLength->size() < overlapped) {
+                insertLength->resize(overlapped + 1);
+            }
+            ++((*insertLength)[overlapped]);
+        }
+        if (overlapped > r1.getLength() && overlapped > r2.getLength()) {
+            ++counters["Lins"];
+        } else {
+            ++counters["Sins"];
+        }
+    }
+ 
+    return overlapped;
+}
+
+/*This is the helper class for overlap
+ * The idea is in the wet lab, they set up sequences to sequences toward each other
+ * Sometimes, these squences can overlap
+ * There are two cases in which they can overlap
+ * They can either overlap just barely on the ends we call - these lins (long insert)
+ * They can also overlap way to much to the point they have adapters
+ * in the read - or a sin (short insert)
+ * With a lin it is useful to have a higher confidence in the bases in the overlap and longer read
+ * With a sin it is useful to have the higher confidence as well as removing the adapters*/
+template <class T, class Impl>
+void helper_overlapper(InputReader<T, Impl> &reader, std::shared_ptr<OutputWriter> pe, std::shared_ptr<OutputWriter> se, Counter& counters, const double misDensity, const size_t minOver, histVec &insertLength, const bool stranded, const size_t min_length, const size_t checkLengths, const size_t kmer, const size_t kmerOffset, bool no_orphan = false ) {
+    
+    while(reader.has_next()) {
+        auto i = reader.next();
+        //Saves check
+        if (insertLength) {
+            if (insertLength->size() < 1) {
+                insertLength->resize(1);
+            }
+        }
+
+        ++counters["TotalRecords"];
+        PairedEndRead* per = dynamic_cast<PairedEndRead*>(i.get());        
+        if (per) {
+            unsigned int overlapped = check_read(*per, counters, misDensity, minOver, insertLength, checkLengths, kmer, kmerOffset, min_length);
+            per->checkDiscarded(min_length);    
+            writer_helper(per, pe, se, stranded, counters, no_orphan); 
+        } else {
+            SingleEndRead* ser = dynamic_cast<SingleEndRead*>(i.get());
+            
+            if (ser) {
+                ser->checkDiscarded(min_length);
+                writer_helper(ser, pe, se, stranded, counters);
+            } else {
+                throw std::runtime_error("Unknown read type");
+            }
+        }
+    }
+}
+
+#endif
+
