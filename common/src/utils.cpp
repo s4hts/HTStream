72dae29a50fea0c80c91ae0e892f223fd69feb69
Matt Settles <mattsettles@gmail.com>
2017-12-27 11:40:12 -0800

    slack issue 12, set default for min-length to 1, effectively turning it off by default

--- common/src/utils.cpp
+++ common/src/utils.cpp
@@ -123,7 +123,7 @@ void setDefaultParamsCutting(po::options_description &desc) {
     desc.add_options()
             ("no-orphans,n", po::bool_switch()->default_value(false), "Orphaned SE reads will NOT be written out")
             ("stranded,s", po::bool_switch()->default_value(false),    "If R1 is orphaned, R2 is output in RC (for stranded RNA)")
-            ("min-length,m", po::value<size_t>()->default_value(50),    "Min length for acceptable output read");
+            ("min-length,m", po::value<size_t>()->default_value(1),    "Min length for acceptable output read");
 
 }
 
