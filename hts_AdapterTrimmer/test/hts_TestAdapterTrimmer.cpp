66bb83faf800162f9e3d62e03cf5f036e3bfa212
David Streett <streett.david@gmail.com>
2018-02-14 14:25:45 -0800

    Forgot arg - should be working now

--- hts_AdapterTrimmer/test/hts_TestAdapterTrimmer.cpp
+++ hts_AdapterTrimmer/test/hts_TestAdapterTrimmer.cpp
@@ -45,8 +45,7 @@ TEST_F(Adapter, issue_126) {
     while(ifp.has_next()) {
         auto i = ifp.next();
         PairedEndRead *per = dynamic_cast<PairedEndRead*>(i.get());
-        ASSERT_EQ( (per->non_const_read_two()).getRTrim(), 0);
-        check_read_pe(*per, misDensity, 8, 20, kmer, kmerOffset, false);
+        check_read_pe(*per, misDensity, 100, 8, 20, kmer, kmerOffset, false);
         const std::string s1 =  "CTTTGTAGGGCAGAAAAATTCTGCCTGACCTTTCCCATCTTCCTGATGATGTTAACAAACGACAGCCACTGAGGGCACAGGAGGGGGCTCTACACAGGCCAAGAGCTACAGGCTACAGTCTGAGCACAGAAAACCCTCCAGCTCCAGC";
         const std::string s2 =  "GCTGGAGCTGGAGGGTTTTCTGTGCTCAGACTGTAGCCTGTAGCTCTTGGCCTGTGTAGAGCCCCCTCCTGTGCCCTCAGTGGCTGTCGTTTGTTAACATCATCAGGAAGATGGGAAAGGTCAGGCAGAATTTTTCTGCCCTACAAAG";
         ASSERT_EQ( (per->non_const_read_one()).get_sub_seq(), s1);
