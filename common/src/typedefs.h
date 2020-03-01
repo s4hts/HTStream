f1b61240e3f1682880b156aa0b04c1f1412f7d88
Matt Settles <mattsettles@gmail.com>
2020-03-01 11:53:48 -0800

    Accidental commit to master, undoing

--- common/src/typedefs.h
+++ common/src/typedefs.h
@@ -7,8 +7,4 @@
 typedef std::tuple <const std::string, uint64_t&> Label;
 typedef std::tuple<uint_fast64_t, uint_fast64_t> Vector;
 
-// seqMatrix is for stats counters, both seq character and seq quality
-// pos, character, count
-typedef std::tuple<uint_fast64_t, const char, uint_fast64_t>  ;
-
 #endif
