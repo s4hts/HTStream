351353bf332ac3b7aa7bd40339a5b758b45bc48f
Joe Angell <joe.d.angell@gmail.com>
2020-03-31 10:05:25 -0700

    adding version.h.release for setting release versions which don't have access to git

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -4,9 +4,16 @@ execute_process(
   COMMAND git describe --tags
   OUTPUT_VARIABLE HTSTREAM_VERSION
   OUTPUT_STRIP_TRAILING_WHITESPACE
+  RESULT_VARIABLE result
 )
-message("execute git version " ${HTSTREAM_VERSION})
 
-
-configure_file(${SRC_DIR}/common/src/version.h.in
-  ${BIN_DIR}/common/version.h @ONLY)
+if(result EQUAL "0")
+  message("execute git version " ${HTSTREAM_VERSION})
+  configure_file(${SRC_DIR}/common/src/version.h.in
+    ${BIN_DIR}/common/version.h @ONLY)
+else()
+  message("using release version")
+  configure_file(${SRC_DIR}/common/src/version.h.release
+    ${BIN_DIR}/common/version.h COPYONLY)
+endif()
+  
