ec5e4fed94072c805a78ea229ec3932f039abb11
Joe Angell <joe.d.angell@gmail.com>
2020-05-10 12:56:03 -0700

    removing sleep, some other issue

--- regression/regression.sh
+++ regression/regression.sh
@@ -23,7 +23,6 @@ testrun() {
         echo running $prog output to $out
         $prog -1 $fastqr1 -2 $fastqr2 -t $out -F
         echo zdiff $out.tab6.gz ${out%%.*}.tab6.gz
-        sleep 1
         zdiff $out.tab6.gz ${out%%.*}.tab6.gz
         rm $out.tab6.gz
     done
