190f91180486926f6af99341e9860e3bba19884b
Matt Settles <mattsettles@gmail.com>
2020-06-14 09:38:28 -0700

    Move check-lengths up on the parameter list because it goes with kmer/kmer-offset

--- common/src/utils.cpp
+++ common/src/utils.cpp
@@ -143,11 +143,11 @@ void setDefaultParamsTrim(po::options_description &desc) {
 
 void setDefaultParamsOverlapping(po::options_description &desc) {
     desc.add_options()
+            ("check-lengths,c", po::value<size_t>()->default_value(20)->notifier(boost::bind(&check_range<size_t>, "check-lengths", _1, 5, 10000)), "Check lengths of the ends (min 5, max 10000)")
             ("kmer,k", po::value<size_t>()->default_value(8)->notifier(boost::bind(&check_range<size_t>, "kmer", _1, 5, 64)), "Kmer size of the lookup table for the longer read (min 5, max 64)")
             ("kmer-offset,r", po::value<size_t>()->default_value(1)->notifier(boost::bind(&check_range<size_t>, "kmer-offset", _1, 1, 10000)), "Offset of kmers. Offset of 1, would be perfect overlapping kmers. An offset of kmer would be non-overlapping kmers that are right next to each other. Must be greater than 0.")
             ("max-mismatch-errorDensity,e", po::value<double>()->default_value(.25)->notifier(boost::bind(&check_range<double>, "max-mismatch-errorDensity", _1, 0.0, 1.0)), "Max percent of mismatches allowed in overlapped section (min 0.0, max 1.0)")
             ("max-mismatch,x", po::value<size_t>()->default_value(100)->notifier(boost::bind(&check_range<size_t>, "max-mismatch", _1, 0, 10000)), "Max number of total mismatches allowed in overlapped section (min 0, max 10000)")
-            ("check-lengths,c", po::value<size_t>()->default_value(20)->notifier(boost::bind(&check_range<size_t>, "check-lengths", _1, 5, 10000)), "Check lengths of the ends (min 5, max 10000)")
             ("min-overlap,o", po::value<size_t>()->default_value(8)->notifier(boost::bind(&check_range<size_t>, "min-length", _1, 5, 10000)), "Min overlap required to merge two reads (min 5, max 10000)");
 }
 
